0298afbb349cd6f2e7ec1d8d4bbbb06d010bec72
anton.a <anton.a@itfox-web.com>
2024-02-20 22:24:28 +0600

    Small fixes

--- meme_generator/lib/main.dart
+++ meme_generator/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meme_generator/cubit/template_cubit.dart';
 import 'package:meme_generator/screen/main_screen.dart';
 
 void main() async {
@@ -14,13 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => TemplateCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MainScreen(),
       ),
-      home: MainScreen(),
     );
   }
 }
